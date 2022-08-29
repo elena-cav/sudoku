a8393b49c756f9f8d28d325fce97dcaa49f1d4c2
Elena Cavallero <elecav@hotmail.it>
2022-08-29 19:28:29 +0200

    complete challenge

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 export default (game) => {
-  const hasDuplicates = (g) => new Set(g).size !== g.length;
+  const hasDuplicates = (g) => g.filter((l, i) => g.lastIndexOf(l) !== i);
   const reversed = [];
-  let isSudoku = true;
+  let columnOrRow = 0;
+  let position = 0;
   let count = 0;
+  const columns = [];
   while (count < 9) {
     game.forEach((row) => reversed.push(row[count]));
     count++;
   }
-  const columns = [];
   while (reversed.length > 0) columns.push(reversed.splice(0, 9));
   const checkIsSudoku = (arr) => {
     for (let i = 0; i < arr.length; i++) {
-      if (hasDuplicates(arr[i])) {
-        isSudoku = false;
-        break;
+      const duplicates = hasDuplicates(arr[i]);
+      if (duplicates.length > 0) {
+        position = arr[i].lastIndexOf(duplicates[0]) + 1;
+        columnOrRow = i + 1;
+        return false;
       }
     }
+    return true;
   };
-  checkIsSudoku(columns) && checkIsSudoku(game);
-  return isSudoku;
+  if (!checkIsSudoku(game)) {
+    return `Sorry, this is not a valid Sudoku. Your error is on row ${columnOrRow}, column ${position}`;
+  } else if (!checkIsSudoku(columns)) {
+    return `Sorry, this is not a valid Sudoku. Your error is on column ${columnOrRow}, row ${position}`;
+  } else return "Congratulations. This is a valid Sudoku!";
 };
--- index.test.js
+++ index.test.js
@@ -13,8 +13,8 @@ describe("sudoku", () => {
     [8, 7, 3, 5, 1, 2, 9, 6, 4],
   ];
   const falseRowSudoku = [
-    [1, 5, 2, 4, 4, 9, 3, 7, 6],
-    [7, 3, 9, 2, 5, 6, 8, 4, 1],
+    [1, 5, 2, 4, 8, 9, 3, 7, 6],
+    [7, 3, 9, 2, 4, 6, 8, 4, 1],
     [4, 6, 8, 3, 7, 1, 2, 9, 5],
     [3, 8, 7, 1, 2, 4, 6, 5, 9],
     [5, 9, 1, 7, 6, 3, 4, 2, 8],
@@ -24,10 +24,10 @@ describe("sudoku", () => {
     [8, 7, 3, 5, 1, 2, 9, 6, 4],
   ];
   const falseColumnSudoku = [
-    [1, 5, 2, 4, 4, 9, 3, 7, 6],
+    [1, 5, 2, 4, 8, 9, 3, 7, 6],
     [7, 3, 9, 2, 5, 6, 8, 4, 1],
     [4, 6, 8, 3, 7, 1, 2, 9, 5],
-    [5, 8, 7, 1, 2, 4, 6, 5, 9],
+    [5, 8, 7, 1, 2, 4, 6, 3, 9],
     [5, 9, 1, 7, 6, 3, 4, 2, 8],
     [2, 4, 6, 8, 9, 5, 7, 1, 3],
     [9, 1, 4, 6, 3, 7, 5, 8, 2],
@@ -35,15 +35,19 @@ describe("sudoku", () => {
     [8, 7, 3, 5, 1, 2, 9, 6, 4],
   ];
   it("should return a boolean", () => {
-    expect(typeof sudoku(trueSudoku)).toBe("boolean");
+    expect(typeof sudoku(trueSudoku)).toBe("string");
   });
   it("should return true if the game is a valid sudoku", () => {
-    expect(sudoku(trueSudoku)).toBe(true);
+    expect(sudoku(trueSudoku)).toBe("Congratulations. This is a valid Sudoku!");
   });
   it("should return false if the game is a valid sudoku", () => {
-    expect(sudoku(falseRowSudoku)).toBe(false);
+    expect(sudoku(falseRowSudoku)).toBe(
+      "Sorry, this is not a valid Sudoku. Your error is on row 2, column 8"
+    );
   });
   it("should return false if the game is a valid sudoku", () => {
-    expect(sudoku(falseColumnSudoku)).toBe(false);
+    expect(sudoku(falseColumnSudoku)).toBe(
+      "Sorry, this is not a valid Sudoku. Your error is on column 1, row 5"
+    );
   });
 });
